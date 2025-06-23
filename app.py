9d2c1327ff8c913fc1ef9eb6319682b7cfaec0ec
Shrushti Dongre <shrushtidongre15@gmail.com>
2025-06-23 13:25:06 +0530

    A Machine Learning Project

--- app.py
+++ app.py
@@ -32,27 +32,27 @@ def home_page():
 
 @app.route("/predict", methods = ['POST'])
 def get_prediction():
+    try:
+        # get data from the user
+        baby_data_form = request.form
+        print("form data recived :" , baby_data_form)
 
-    # get data from the user
-    baby_data_form = request.form
+        baby_data_cleaned = get_cleaned_data(baby_data_form)
+        
+        # convert it itno dataframe
+        baby_df = pd.DataFrame(baby_data_cleaned)
 
-    baby_data_cleaned = get_cleaned_data(baby_data_form)
-    
-    # convert it itno dataframe
-    baby_df = pd.DataFrame(baby_data_cleaned)
+        # load machine learning model
+        with open('model.pkl','rb') as obj:
+            model = pickle.load(obj) 
 
-    # load machine learning model
-    with open('model.pkl','rb') as obj:
-       model = pickle.load(obj) 
+        # make prediction
+        prediction = model.predict(baby_df)
+        prediction = round(float(prediction),2)
 
-    # make prediction
-    prediction = model.predict(baby_df)
-    prediction = round(float(prediction),2)
-
-    #return response in a json format
-    response = {"Prediction":prediction}
-
-    return render_template("index.html",prediction = prediction)
-
+        return render_template("index.html",prediction = prediction)
+    except Exception as e :
+        print("Error in ?predict",e )
+        return f"Error:{e}"
 if __name__ == "__main__":
     app.run(debug = True)
